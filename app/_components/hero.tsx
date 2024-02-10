cf735a9c217607bfacd66d166e0718430b29e295
AhmadBShaik <ahmad.shaik1106@gmail.com>
2024-02-10 12:22:56 +0530

    fix issue #1

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <div className="min-h-[calc(100vh-6rem)] flex flex-col relative">
+    <div className="max-h-[calc(100vh-6rem)] flex flex-col relative py-8 px-4 2xl:px-0">
       <div className="md:grid md:grid-cols-2 h-full flex-1 min-w-96">
         <div className="relative aspect-[693/598]">
           <Image
@@ -11,26 +11,17 @@ export const Hero = () => {
             fill
             className="md:hidden absolute w-full h-full"
           />
-          <div className="sm:hidden absolute bg-white left-0 right-0  -bottom-32 bg-opacity-50 flex justify-center items-center">
-            <div className="max-w-96 w-96 border  p-2.5 rounded-lg md:border-none  backdrop-blur-md md:backdrop-blur-none ">
-              <div className="text-3xl font-bold">
+          <div className="hidden md:flex absolute bg-white right-5  sm:left-none -bottom-32 md:top-0 md:right-0 md:bottom-0 md:left-0 bg-opacity-50  items-center">
+            <div className="p-2.5 rounded-lg ">
+              <div className="text-2xl lg:text-3xl xl:text-4xl font-bold">
                 Virtual Healthcare for you
               </div>
-
-              <div className="text-gray-500 mt-2">
+              <div className="text-gray-500 mt-2 lg:text-lg xl:text-xl">
                 Trafalgar provides progressive, and affordable healthcare,
-                accessible on mobile and online for everyone Consult today
+                accessible on mobile and online for everyone
               </div>
-            </div>
-          </div>
-          <div className="hidden sm:flex absolute bg-white right-5  sm:left-none -bottom-32 md:top-0 md:right-0 md:bottom-0 md:left-0 bg-opacity-50 justify-center items-center">
-            <div className="max-w-96 w-96 border p-2.5 rounded-lg backdrop-blur-md md:backdrop-blur-none border md:border-none">
-              <div className="text-3xl font-bold">
-                Virtual Healthcare for you
-              </div>
-              <div className="text-gray-500 mt-2">
-                Trafalgar provides progressive, and affordable healthcare,
-                accessible on mobile and online for everyone Consult today
+              <div className="bg-blue-400 text-white font-bold text-xl px-4 pt-2.5 pb-3.5 flex justify-center items-center mt-5 w-fit rounded-full">
+                Consult today
               </div>
             </div>
           </div>
@@ -44,6 +35,18 @@ export const Hero = () => {
           />
         </div>
       </div>
+      <div className="flex md:hidden  bg-white right-5  sm:left-none -bottom-32 md:top-0 md:right-0 md:bottom-0 md:left-0 bg-opacity-50 justify-center items-center pt-4">
+        <div className="flex flex-col items-center text-center p-2.5 rounded-lg ">
+          <div className="text-4xl font-bold">Virtual Healthcare for you</div>
+          <div className="text-gray-500 mt-2">
+            Trafalgar provides progressive, and affordable healthcare,
+            accessible on mobile and online for everyone
+          </div>
+          <div className="bg-blue-400 text-white font-bold text-xl px-4 pt-2.5 pb-3.5 flex justify-center items-center mt-5 w-fit rounded-full">
+            Consult today
+          </div>
+        </div>
+      </div>{" "}
     </div>
   );
 };
